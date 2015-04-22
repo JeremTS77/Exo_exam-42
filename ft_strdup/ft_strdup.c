9d5e385f306ec73990b0dfb792db0e35d3ab58e6
Jeremy LEFEBVRE--AGNES <jeremy.la@me.com>
2015-04-22 21:44:02 +0200

    Exam du 18/12/2014

---
+++ ft_strdup/ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/18 09:43:02 by exam              #+#    #+#             */
+/*   Updated: 2014/12/18 09:53:12 by exam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int n;
+
+	n = 0;
+	while (str[n] != '\0')
+		n++;
+	return (n);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		index;
+
+	index = 0;
+	if (!(dest = (char *)malloc(sizeof(char) * ft_strlen(src) + 1)))
+		return ((void *)0);
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
