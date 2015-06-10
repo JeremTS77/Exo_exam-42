2fd45e36d0d9618ed96a53df26259d88b15dbb47
JeremTS77 <JeremTS77@users.noreply.github.com>
2015-06-10 09:45:43 +0200

    Ca marche !

--- Other/sort_int_tab/sort_int_tab.c
+++ Other/sort_int_tab/sort_int_tab.c
@@ -31,3 +31,26 @@ void	sort_int_tab(int *tab, unsigned int size)
 		}
 	}
 }
+
+#include <stdio.h>
+
+int main(void)
+{
+    int tab[10];
+    int i = 0;
+
+    tab[0] = 9;
+    tab[1] = 2;
+    tab[2] = 4;
+    tab[3] = 3;
+    tab[4] = 8;
+    tab[5] = 0;
+    tab[6] = 1;
+    tab[7] = 7;
+    tab[8] = 5;
+    tab[9] = 6;
+    sort_int_tab(tab, 9);
+    while (i <= 9)
+        printf("%d\n", tab[i++]);
+    return (0);
+}
